9cc8302c6330e8ca131963347669a16f942a7b63
Ananay-22 <guptavananay@gmail.com>
2022-12-09 05:55:01 -0500

    jni cmake

---
+++ run-java.sh
@@ -0,0 +1,18 @@
+modulePath="/usr/share/openjfx/lib"
+outputDirectory="./build/"
+headerOutputDirectory="./pi-ulcd-jni/"
+declare -a javafxModules=(
+    "javafx.base"
+    "javafx.graphics"
+    "javafx.media"
+    )
+javaClasspath=""
+for val in ${javafxModules[@]}; do
+  javaClasspath="$val.jar;$javaClasspath"
+done
+addModules=""
+for val in ${javafxModules[@]}; do
+  addModules="$val,$addModules"
+done
+# java --module-path /usr/share/openjfx/lib --add-modules javafx.base,javafx.graphics,javafx.media -classpath "com.nana;/usr/share/openjfx/lib/javafx.graphics.jar" ./com/nana/uLCDInterface.java
+java -Djava.library.path=./ --module-path $modulePath --add-modules $addModules -classpath "com.nana;$javaClasspath" ./com/nana/uLCDInterface.java
