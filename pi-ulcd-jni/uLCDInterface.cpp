d4a7dd9a3b71c5c20da87aaa6e001636a86d1cde
Ananay-22 <guptavananay@gmail.com>
2022-12-09 12:58:37 -0500

    test java interface

--- pi-ulcd-jni/uLCDInterface.cpp
+++ pi-ulcd-jni/uLCDInterface.cpp
@@ -64,7 +64,7 @@ JNIEXPORT jboolean JNICALL Java_com_nana_uLCDInterface_internalWriteImageToULCD 
                 // printf(" failed. Retrying...\n");
                 // fflush(stdout);
             }
-            wait_us(50);
+            wait_us(75);
             // printf(" done\n");
         }
     }
