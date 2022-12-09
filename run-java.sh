68f65ab9b7aed1c972cbcbc86b8ba9d9f5f03b16
Ananay-22 <guptavananay@gmail.com>
2022-12-09 06:05:16 -0500

    jni cmake

--- run-java.sh
+++ run-java.sh
@@ -15,4 +15,5 @@ for val in ${javafxModules[@]}; do
   addModules="$val,$addModules"
 done
 # java --module-path /usr/share/openjfx/lib --add-modules javafx.base,javafx.graphics,javafx.media -classpath "com.nana;/usr/share/openjfx/lib/javafx.graphics.jar" ./com/nana/uLCDInterface.java
+echo java -Djava.library.path=./ --module-path $modulePath --add-modules $addModules -classpath "com.nana;$javaClasspath" com.nana.uLCDInterface
 java -Djava.library.path=./ --module-path $modulePath --add-modules $addModules -classpath "com.nana;$javaClasspath" com.nana.uLCDInterface
