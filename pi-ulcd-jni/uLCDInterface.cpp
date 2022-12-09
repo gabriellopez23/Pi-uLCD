87e28a4b2c1eda51c88a53c7c53bb72cc990ce08
Ananay-22 <guptavananay@gmail.com>
2022-12-09 07:56:08 -0500

    jni cmake

--- pi-ulcd-jni/uLCDInterface.cpp
+++ pi-ulcd-jni/uLCDInterface.cpp
@@ -3,44 +3,44 @@
 #include <cstdint>
 
 JNIEXPORT jboolean JNICALL Java_com_nana_uLCDInterface_internalWriteImageToULCD (JNIEnv * env, jclass java_class, jint sector_start, jobjectArray img) {
-    // uLCD_4DGL uLCD;
+    uLCD_4DGL uLCD;
 
-    // int height = env->GetArrayLength(img);
-    // int width  = env->GetArrayLength((jshortArray) env->GetObjectArrayElement(img, 0));
+    int height = env->GetArrayLength(img);
+    int width  = env->GetArrayLength((jshortArray) env->GetObjectArrayElement(img, 0));
 
-    // uint32_t sector_address = static_cast<uint32_t>(sector_start);
+    uint32_t sector_address = static_cast<uint32_t>(sector_start);
 
-    // printf("init uLCD media\n");
+    printf("init uLCD media\n");
 
-    // uLCD.media_init();
-    // uLCD.set_sector_address(
-    //     ((sector_address >> 16) & 0xFFFF), 
-    //     ((sector_address >>  0) & 0xFFFF)
-    // );
+    uLCD.media_init();
+    uLCD.set_sector_address(
+        ((sector_address >> 16) & 0xFFFF), 
+        ((sector_address >>  0) & 0xFFFF)
+    );
 
-    // for (int row = 0; row < height; row++) {
-    //     printf("checking row widths: %d", row);
-    //     if (env->GetArrayLength((jshortArray) env->GetObjectArrayElement(img, row)) != width) {
-    //         return static_cast<jboolean>(false);
-    //     printf("done\n");
-    //     }
-    // }
+    for (int row = 0; row < height; row++) {
+        printf("checking row widths: %d", row);
+        if (env->GetArrayLength((jshortArray) env->GetObjectArrayElement(img, row)) != width) {
+            return static_cast<jboolean>(false);
+        printf("done\n");
+        }
+    }
 
-    // for (int row = 0; row < height; row++) {
-    //     for (int col = 0; col < width; col++) {
-    //         printf("writing pixel: %d %d", row, col);
-    //         // jshort pixel_data = (
-    //         //         ((jshort*)      env->GetShortArrayElements(
-    //         //         (jshortArray) env->GetObjectArrayElement(img, row),
-    //         //                       nullptr
-    //         //     ))[col]);
-    //         // uLCD.write_byte(static_cast<int>(pixel_data));
-    //         printf("done\n");
-    //     }
-    // }
+    for (int row = 0; row < height; row++) {
+        for (int col = 0; col < width; col++) {
+            printf("writing pixel: %d %d", row, col);
+            jshort pixel_data = (
+                    ((jshort*)      env->GetShortArrayElements(
+                    (jshortArray) env->GetObjectArrayElement(img, row),
+                                  nullptr
+                ))[col]);
+            uLCD.write_byte(static_cast<int>(pixel_data));
+            printf("done\n");
+        }
+    }
 
-    // printf("flushing media");
-    // uLCD.flush_media();
+    printf("flushing media");
+    uLCD.flush_media();
     printf("returning true.\n");
 
     return static_cast<jboolean>(true);
