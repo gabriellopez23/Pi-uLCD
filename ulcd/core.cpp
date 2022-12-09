4c913bcf03fe8602e4c193b665acef67b18af709
Ananay-22 <guptavananay@gmail.com>
2022-12-09 00:21:10 -0500

    new printf implementation

--- ulcd/core.cpp
+++ ulcd/core.cpp
@@ -475,7 +475,7 @@ int uLCD_4DGL::printf(const char *fmt, ...) {
     if (buffer == 0) return -1;
     sprintf(buffer, fmt, args);
     for (int i = 0; i < buffer_size; i++) {
-        printf("%d", buffer[i]);
+        std::printf("%c", buffer[i]);
         _putc(buffer[i]);
     }
     free(buffer);
