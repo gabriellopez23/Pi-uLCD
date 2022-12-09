3def81fc9003c5da4e163b75f7ba6b66c186547a
Ananay-22 <guptavananay@gmail.com>
2022-12-09 04:54:51 -0500

    configuring java lib

--- build-java.sh
+++ build-java.sh
@@ -1,1 +1,1 @@
-java -cp "com.nana;/usr/share/openjfx/lib/javafx.graphics.jar" ./com/nana/uLCDInterface.java --module-path /usr/share/openjfx/lib --add-module javafx.base,javafx.graphics,javafx.media
+java --module-path /usr/share/openjfx/lib --add-module javafx.base,javafx.graphics,javafx.media -classpath "com.nana;/usr/share/openjfx/lib/javafx.graphics.jar" ./com/nana/uLCDInterface.java
