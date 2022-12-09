93e0769f5f21a2f0e17808c7023a32564e9661ad
Ananay-22 <guptavananay@gmail.com>
2022-12-09 04:08:43 -0500

    add in image operations

---
+++ java.mk
@@ -0,0 +1,26 @@
+SOURCE_PATH = "../com/nana"
+JFLAGS = -g
+JC = javac
+JVM= java 
+# FILE=
+
+.SUFFIXES: .java .class
+
+.java.class:
+        $(JC) $(JFLAGS) $(SOURCE_PATH)$*.java
+
+CLASSES = \
+        uLCDInterface.java
+
+MAIN = uLCDInterface 
+
+default: classes
+
+
+classes: $(CLASSES:.java=.class)
+
+run: $(MAIN).class
+	$(JVM) $(MAIN) $(FILE)
+
+clean:
+        $(RM) *.class
