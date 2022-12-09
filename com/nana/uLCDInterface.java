de507eb063e06ebb66f04f129f8d777ec8b18fc3
Ananay-22 <guptavananay@gmail.com>
2022-12-09 06:37:53 -0500

    jni cmake

--- com/nana/uLCDInterface.java
+++ com/nana/uLCDInterface.java
@@ -14,28 +14,28 @@ public final class uLCDInterface {
         public int getIntColor(int row, int col);
     }
 
-    private static final <T> byte[][] convertImage(int width, int height, T imgSrc, ImageConverter converter) {
-      byte[][] result = new byte[height][width * 4];
+    private static final <T> short[][] convertImage(int width, int height, T imgSrc, ImageConverter converter) {
+        short[][] result = new short[height][width];
 
       for (int row = 0; row < height; row++) {
          for (int col = 0; col < width; col++) {
             int imgPixel = converter.getIntColor(row, col);
-            result[row][col    ] = (byte) (                      0);
-            result[row][col + 1] = (byte) ((imgPixel) >> 16 & 0xFF);
-            result[row][col + 2] = (byte) ((imgPixel) >>  8 & 0xFF);
-            result[row][col + 3] = (byte) ((imgPixel) >>  0 & 0xFF);
+            result[row][col] = (short) ((((imgPixel >> 16) & 0x1F) << 11) 
+                                      | (((imgPixel >>  8) & 0x3F) <<  5) 
+                                      | (((imgPixel >>  0) & 0x1F) <<  0)
+                                       );
          }
       }
       return result;
     }
 
-    public static final byte[][] imageToRAW(BufferedImage img) {
+    public static final short[][] imageToRAW(BufferedImage img) {
         int width = img.getWidth();
         int height = img.getHeight();
         return convertImage(width, height, img, img::getRGB);
     }
 
-    public static final byte[][] imageToRAW(WritableImage img) {
+    public static final short[][] imageToRAW(WritableImage img) {
         int width = (int) img.getWidth();
         int height = (int) img.getHeight();
         return convertImage(width, height, img, img.getPixelReader()::getArgb);
--- pi-ulcd-jni/uLCDInterface.cpp
+++ pi-ulcd-jni/uLCDInterface.cpp
@@ -1,6 +1,33 @@
 #include "com_nana_uLCDInterface.h"
 #include "ulcd.h"
+#include <cstdint>
 
 JNIEXPORT jboolean JNICALL Java_com_nana_uLCDInterface_writeImageToULCD (JNIEnv * env, jclass java_class, jint sector_start, jobjectArray img) {
+    uLCD_4DGL uLCD;
+
+    int height = javaEnvironment->GetArrayLength(img);
+    int width  = javaEnvironment->GetArrayLength((jshortArray) javaEnvironment->GetObjectArrayElement(img, 0));
+
+    uint32_t sector_address = static_cast<uint32_t>(sector_start);
+
+    uLCD.media_init();
+    uLCD.set_sector_address(
+        ((sector_address >> 16) & 0xFFFF), 
+        ((sector_address >>  0) & 0xFFFF)
+    );
+
+    for (int row = 0; row < height; row++) {
+        for (int col = 0; col < width; col++) {
+            uint16_t pixel_data = static_cast<uint16_t>(
+                (jshort) javaEnvironment->GetObjectArrayElement(
+                    (jshortArray) javaEnvironment->GetObjectArrayElement(img, row),
+                                  col
+                ));
+            uLCD.write_byte(static_cast<int>(pixel_data));
+        }
+    }
+
+    uLCD.flush_media();
+
     return static_cast<jboolean>(true);
 }
