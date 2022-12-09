bf6aa3d11716fce53a396526802c0be7bc75789b
Ananay-22 <guptavananay@gmail.com>
2022-12-08 23:00:37 -0500

    add demo printf

--- ulcd/main.cpp
+++ ulcd/main.cpp
@@ -469,7 +469,7 @@ int uLCD_4DGL :: getSTATUS(char *command, int number)   // read screen info and 
 
 int main(int argc, char* argv) {
     uLCD_4DGL ulcd();
-    ulcd.printf("some text");
+    ulcd.line(10, 10, 10, 10, WHITE);
     while (1) {}
     return 0;
 }
