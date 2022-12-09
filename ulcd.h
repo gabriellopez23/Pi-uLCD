207c60041396c84d202bee37d2eeec2445eb0b9b
Ananay-22 <guptavananay@gmail.com>
2022-12-08 22:54:38 -0500

    change serial interface for serial

--- ulcd.h
+++ ulcd.h
@@ -207,7 +207,7 @@ class uLCD_4DGL
  
 public :
     // TODO: change ints to Pinname
-    uLCD_4DGL(char* sertty = "/dev/ttyAMA0", int rst = 17);
+    uLCD_4DGL(char* sertty = " /dev/ttyS0", int rst = 17);
  
 // General Commands *******************************************************************************
  
