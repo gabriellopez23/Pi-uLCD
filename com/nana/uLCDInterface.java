7e25dc4fa5f46cca54d089e8fefdecaba020a3b8
Ananay-22 <guptavananay@gmail.com>
2022-12-09 11:45:56 -0500

    jni cmake

--- com/nana/uLCDInterface.java
+++ com/nana/uLCDInterface.java
@@ -35,9 +35,9 @@ public final class uLCDInterface {
       for (int row = 0; row < height; row++) {
          for (int col = 0; col < width; col++) {
             int imgPixel = converter.getIntColor(row, col);
-            result[row][col] = (short) ((((imgPixel >> 16) & 0x1F) <<  0) 
+            result[row][col] = (short) ((((imgPixel >> 16) & 0x1F) << 11) 
                                       | (((imgPixel >>  8) & 0x3F) <<  5) 
-                                      | (((imgPixel >>  0) & 0x1F) << 11)
+                                      | (((imgPixel >>  0) & 0x1F) <<  0)
                                        );
          }
       }
