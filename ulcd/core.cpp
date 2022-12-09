5cdcc6936d9812c7a30f906dd6aefa567a756635
Ananay-22 <guptavananay@gmail.com>
2022-12-09 10:11:41 -0500

    jni cmake

--- ulcd/core.cpp
+++ ulcd/core.cpp
@@ -121,6 +121,8 @@ int uLCD_4DGL :: writeCOMMAND(char *command, int number)   // send several BYTES
 #if DEBUGMODE
     pc.printf("   Answer received : %d\n",resp);
 #endif
+
+    wait_ms(1);
  
     return resp;
 }
