925d2f2d328d7efafa199a2eb790f2c7d320d1ed
Ananay-22 <guptavananay@gmail.com>
2022-12-09 05:06:04 -0500

    java build script

--- build-java.sh
+++ build-java.sh
@@ -7,11 +7,11 @@ declare -a javafxModules=(
     )
 javaClasspath=""
 for val in ${javafxModules[@]}; do
-  javaClasspath="$val;$javaClasspath"
+  javaClasspath="$val.jar;$javaClasspath"
 done
 addModules=""
 for val in ${javafxModules[@]}; do
-  addModules="$val.jar;$addModules"
+  addModules="$val;$addModules"
 done
 # java --module-path /usr/share/openjfx/lib --add-modules javafx.base,javafx.graphics,javafx.media -classpath "com.nana;/usr/share/openjfx/lib/javafx.graphics.jar" ./com/nana/uLCDInterface.java
 javac --module-path $modulePath --add-modules $addModules -classpath "com.nana;$javaClasspath" ./com/nana/uLCDInterface.java
