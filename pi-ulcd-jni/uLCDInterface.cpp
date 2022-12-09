b61c34ae7a8090731ce269cac41d571b14e02a4c
Ananay-22 <guptavananay@gmail.com>
2022-12-09 06:39:22 -0500

    jni cmake

--- pi-ulcd-jni/uLCDInterface.cpp
+++ pi-ulcd-jni/uLCDInterface.cpp
@@ -19,7 +19,7 @@ JNIEXPORT jboolean JNICALL Java_com_nana_uLCDInterface_writeImageToULCD (JNIEnv 
     for (int row = 0; row < height; row++) {
         for (int col = 0; col < width; col++) {
             uint16_t pixel_data = static_cast<uint16_t>(
-                    (jshort)      env->GetObjectArrayElement(
+                    (jshort)      env->GetShortArrayElement(
                     (jshortArray) env->GetObjectArrayElement(img, row),
                                   col
                 ));
